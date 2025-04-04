dbaa2616fff9096c31b47247f8bb718fb2081957
tmb2k01 <tmb2k01@gmail.com>
2025-04-04 11:04:18 +0200

    download data from github

--- scripts/prepare-data.sh
+++ scripts/prepare-data.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 cd "$(dirname "$0")/.."
+if [ ! -d "data" ]; then
+    mkdir -p data
+fi
+curl -L -o data/MDC_dataset.tar.gz https://github.com/pappd90/mdc_dataset/raw/main/data/MDC_dataset.tar.gz
 tar -xzf data/MDC_dataset.tar.gz -C data
