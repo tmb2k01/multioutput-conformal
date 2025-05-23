4042e73ee8b0ac7fda94551de4fc6d41c535b968
tmb2k01 <tmb2k01@gmail.com>
2025-05-23 11:01:14 +0200

    UTKFaces preprocess

--- scripts/prepare-data.sh
+++ scripts/prepare-data.sh
@@ -25,6 +25,9 @@ if command -v python &>/dev/null; then
 else
     python3 scripts/download_utkfaces.py
 fi
+if [ ! -d "data/UTKFaces" ]; then
+    mkdir -p data/UTKFaces
+fi
 tar -xzf data/UTKFaces_dataset_1.tar.gz -C data/UTKFaces
 tar -xzf data/UTKFaces_dataset_2.tar.gz -C data/UTKFaces
 tar -xzf data/UTKFaces_dataset_3.tar.gz -C data/UTKFaces
@@ -32,9 +35,9 @@ tar -xzf data/UTKFaces_dataset_3.tar.gz -C data/UTKFaces
 
 
 if command -v python &>/dev/null; then
-    #python scripts/prepare-sgvehicle-dataset.py
-    #python scripts/prepare-utkfaces-dataset.py
+    python scripts/prepare-sgvehicle-dataset.py
+    python scripts/prepare-utkfaces-dataset.py
 else
-    #python3 scripts/prepare-sgvehicle-dataset.py
-    #python3 scripts/prepare-utkfaces-dataset.py
+    python3 scripts/prepare-sgvehicle-dataset.py
+    python3 scripts/prepare-utkfaces-dataset.py
 fi
